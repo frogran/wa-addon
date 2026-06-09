a316bd0b0f23f2f4146a42f3cee69332fe37abb2
Gal Gorfung <galgo@mac.mynetworksettings.com>
2026-06-09 12:01:50 -0400

    feat: wire scheduler into app startup

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ require('dotenv').config();
 const db = require('./src/db');
 const server = require('./src/server');
 const bridge = require('./src/bridge');
+const scheduler = require('./src/scheduler');
 
 db.init();
 server.init();
 bridge.init();
+scheduler.init(bridge.sendMessage);
